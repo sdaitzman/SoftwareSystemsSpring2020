9a55896e8b33bcc094ffdba7862a1c1e50efd872
Sam Daitzman <sam@daitzman.com>
2020-01-23 20:54:55 -0500

    working hello world

---
+++ cards/cards.c
@@ -0,0 +1,12 @@
+/*
+* Program to evaluate card face values.
+* Probably not released under any license.
+* (c) 2014 The College Blackjack Team
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Hello world");
+}
